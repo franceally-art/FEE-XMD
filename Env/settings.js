be1656f086a9d1122f45aa7504e2e93d91d84137
franceally-art <franceally36@gmail.com>
2026-06-10 11:32:05 +0300

    Update settings.js

--- Env/settings.js
+++ Env/settings.js
@@ -1,6 +1,6 @@
  const session = process.env.SESSION || '';
 const mycode = process.env.CODE || "255";
-const botname = process.env.BOTNAME || '𝙁𝙀𝙀-𝙓𝙈𝘿';
+const botname = process.env.BOTNAME || 'France_Bot';
 const herokuAppName = process.env.HEROKU_APP_NAME || '';
 const herokuApiKey = process.env.HEROKU_API_KEY || '';
 const database = process.env.DATABASE_URL || '';
